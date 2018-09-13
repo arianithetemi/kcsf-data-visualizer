92e83d779fd7bfb33871712678b1699ae0e0327d
Dorron Zherka <dorron.zherka@gmail.com>
2018-09-13 16:06:15 +0200

    Improve visualizer

--- static/js/auth.js
+++ static/js/auth.js
@@ -5,153 +5,141 @@ var APIBaseUrl = {{ site.requesturl }}
 
 $(document).ready(() => {
    resetPasswordDOM();
-   changePasswordAction();
-   authenticate();
-   resetPasswordAction();
-});
-function authenticate() 
-{
-   $('.authenticate').on('click', e => {
-      e.preventDefault();
-      // console.log($('.loginButton').hasClass('authenticate'))
-         // return false;
 
-      var data = {
-         email: $('#email').val(),
-         password: $('#password').val()
-      }
-
-      if(data.email == "" && data.password == "") {
-        swal({
-            type: 'warning',
-            title: 'Kujdes!',
-            text: 'Ju lutem shënoni email dhe fjalëkalimin',
-         })
-      } else if ($('#email').val() === "" ) {
-        swal({
-            type: 'warning',
-            title: 'Kujdes!',
-            text: 'Ju lutem shënoni email',
-         })
-      } else if (data.password == "") {
-        swal({
-            type: 'warning',
-            title: 'Kujdes!',
-            text: 'Ju lutem shënoni fjalëkalimin',
-         })
-      } else {
-        $.ajax({
-            url: APIBaseUrl + '/login',
-            type: 'POST',
-            dataType: 'json',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            crossDomain: true,
-            data: JSON.stringify(data),
-            success: data => {
-               if (data.success == true) {
-                  window.location.href = "{{ site.basepath }}/dashboard";
-                  localStorage.setItem('token', data.token);
-               } else {
-                  swal({
-                    type: 'error',
-                    title: 'Gabim',
-                    text: data.msg,
-                 }).then((result) => {
-                    if (result.value && data.success) {
-                       $('#import-data-form').trigger("reset");
-                    }
-                 });
-               }
-            }
-         })
-      }
-
-      
-   });
-
-}
-
-/*
-* It manipulates login form in order to proceed with reset password functionality
-*/
-function resetPasswordDOM()
-{
-   $('#resetPasswordLink').on('click', function(e) {
-      e.preventDefault();
-      $('#login').addClass('hideItem')
-      $('#resetPassword').removeClass('hideItem')
-   })
-
-   $('#goToLoginForm').on('click', function(e)
-   {
-      e.preventDefault();
-      $('#resetPassword').addClass('hideItem')
-      $('#login').removeClass('hideItem')
-   })
-}
-
-/*
-* Send email to user with unique link to reset password
-*/
-function resetPasswordAction()
-{
-   $('.resetPasswordButton').on('click', function(e) {
-      e.preventDefault();
-
-      let data = {
-         email: $('#emailForgotPassword').val()
-      }
-      if ($('#emailForgotPassword').val() === "" ) {
-        swal({
-            type: 'warning',
-            title: 'Kujdes!',
-            text: 'Ju lutem shënoni email',
-         })
-      } else {
-        swal({
-            title: 'Duke kontrullar!',
+   $('#login').validate({
+    rules: {
+        email:{
+            required: true,
+            email: true,
+        },
+        password: {
+            required: true
+        }
+    },
+    messages: {
+        email : {
+            required: "Ju lutem shënoni email",
+            email: "Nuk është email valid"
+        },
+        password: {
+            required: "Ju lutem shënoni fjalëkalimin"
+        }
+    },
+    submitHandler: () => {
+        var data = {
+            email: $('#email').val(),
+            password: $('#password').val()
+         }
+         swal({
+            title: 'Duke u kycur!',
             onOpen: () => {
                 swal.showLoading()
             }
         });
-          $.ajax({
-             url: APIBaseUrl + '/forgot_password',
-             type: 'POST',
-             dataType: 'json',
-             headers: {
-                 'Content-Type': 'application/json'
-             },
-             crossDomain: true,
-             data: JSON.stringify(data),
-             success: data => {
-                if (data.success == true) {
-                   swal("Sukses!",data.msg,"success");
-                } else {
-                    swal("Gabim!","Email është gabim!","error");
+           $.ajax({
+               url: APIBaseUrl + '/login',
+               type: 'POST',
+               dataType: 'json',
+               headers: {
+                   'Content-Type': 'application/json'
+               },
+               crossDomain: true,
+               data: JSON.stringify(data),
+               success: data => {
+                  if (data.success == true) {
+                     window.location.href = "{{ site.basepath }}/dashboard";
+                     localStorage.setItem('token', data.token);
+                  } else {
+                     swal({
+                       type: 'error',
+                       title: 'Gabim',
+                       text: data.msg,
+                    }).then((result) => {
+                       if (result.value && data.success) {
+                          $('#import-data-form').trigger("reset");
+                       }
+                    });
+                  }
+               }
+            })
+    },
+})
+$('#resetPassword').validate({
+    rules:{
+        email:{
+            required: true,
+            email: true,
+        }
+    },
+    messages:{
+        email : {
+            required: "Ju lutem shënoni email",
+            email: "Nuk është email valid"
+        },
+    },
+    submitHandler : () => {
+        let data = {
+            email: $('#emailForgotPassword').val()
+         }
+        
+           swal({
+               title: 'Duke kontrullar!',
+               onOpen: () => {
+                   swal.showLoading()
+               }
+           });
+             $.ajax({
+                url: APIBaseUrl + '/forgot_password',
+                type: 'POST',
+                dataType: 'json',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                crossDomain: true,
+                data: JSON.stringify(data),
+                success: data => {
+                   if (data.success == true) {
+                      swal("Sukses!",data.msg,"success");
+                   } else {
+                       swal("Gabim!","Email është gabim!","error");
+                   }
                 }
-             }
-          })
-      }
-      
-   })
-}
+             })
+    }
+})
 
-function changePasswordAction() 
-{
-   $('#submitNewPassword').on('click', function(e) {
-      e.preventDefault();
-
-
-      let code = findGetParameter('code');
+$("#change_password").validate({
+    rules:{
+        newPassword: {
+            required: true,
+            minlength: 6
+        },
+        retypePassword: {
+            required: true,
+            minlength: 6,
+            equalTo: "#password"
+        }
+    },
+    messages:{
+        newPassword: {
+            required: "Ju lutem shënoni fjalëkalimin",
+            minlength: "Fjalëkalimi duhet të mbi 6 karaktere"
+        },
+        retypePassword: {
+            required: "Ju lutem rishënoni fjalëkalimin",
+            minlength: "Fjalëkalimi duhet të mbi 6 karaktere",
+            equalTo: "Fjalëkalimet nuk përputhen"
+        }
+    },
+    submitHandler : () => {
+        let code = findGetParameter('code');
 
       let data = {
          newPassword: $('#password').val(),
          repeatNewPassword: $('#retypePassword').val(),
          code: code
       }
-      if(data.newPassword == data.repeatNewPassword) {
+     
         swal({
             title: 'Duke ndryshuar fjalëkalimin!',
             onOpen: () => {
@@ -179,12 +167,40 @@ function changePasswordAction()
                }
             }
          })
-      } else {
-        swal("Gabim!","Fjalëkalimet nuk përputhen!","error");
-      }
+    }
+})
+});
+
+/*
+* It manipulates login form in order to proceed with reset password functionality
+*/
+function resetPasswordDOM()
+{
+   $('#resetPasswordLink').on('click', function(e) {
+      e.preventDefault();
+      $('#login').addClass('hideItem')
+      $('#resetPassword').removeClass('hideItem')
    })
 
+   $('#goToLoginForm').on('click', function(e)
+   {
+      e.preventDefault();
+      $('#resetPassword').addClass('hideItem')
+      $('#login').removeClass('hideItem')
+   })
 }
+
+/*
+* Send email to user with unique link to reset password
+*/
+function resetPasswordAction()
+{
+   $('.resetPasswordButton').on('click', function(e) {
+      e.preventDefault();
+   })
+}
+
+
 
 
 function checkIfDateExpired() {
